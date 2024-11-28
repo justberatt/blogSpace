df552417dbe0ca9c79b1b05952d9a48014514985
Berat Sakipi <berat.sakipi@gmail.com>
2024-11-28 23:07:45 +0100

    Getting the input from the form, pushing it to the database and updating the UI with the new post

--- index.js
+++ index.js
@@ -8,7 +8,38 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
                 <h3>${post.title}</h3>
                 <p>${post.body}</p>
                 <hr />
-            `
+                `
         }
         document.getElementById("blog-list").innerHTML = html
     })
+
+const form = document.querySelector('#form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const data = new FormData(form);
+    const formDataObject = {
+            title: data.get("post-title"),
+            body: data.get("post-body"),
+        };
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(formDataObject),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data =>  {
+            document.querySelector('#blog-list').innerHTML += `
+                <h3>${data.title}</h3>
+                <p>${data.body}</p>
+                <hr />
+                `
+            const titleInput = document.querySelector('#post-title');
+            titleInput.value = ''
+            const bodyInput = document.querySelector('#post-body');
+            bodyInput.value = ''
+        })
+})
